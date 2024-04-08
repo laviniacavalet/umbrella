08bd3ce36cf451f7f012b8daaaec0ba2fd47481a
laviniacavalet <116130863+laviniacavalet@users.noreply.github.com>
2024-04-08 23:53:33 +0000

    final version

--- umbrella.rb
+++ umbrella.rb
@@ -27,20 +27,22 @@ loc = geo.fetch("location")
 pp latitude = loc.fetch("lat")
 pp longitude = loc.fetch("lng")
 
+puts "Your coordinates are #{latitude}, #{longitude}."
+
 
 # Hidden variables
-#pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER")
+pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER")
 
 # Assemble the full URL string by adding the first part, the API token, and the last part together
-#pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
 
 # Place a GET request to the URL
-#raw_response = HTTP.get(pirate_weather_url)
+raw_response = HTTP.get(pirate_weather_url)
 
-#parsed_response = JSON.parse(raw_response)
+parsed_response = JSON.parse(raw_response)
 
-#currently_hash = parsed_response.fetch("currently")
+currently_hash = parsed_response.fetch("currently")
 
-#current_temp = currently_hash.fetch("temperature")
+current_temp = currently_hash.fetch("temperature")
 
-#puts "The current temperature is " + current_temp.to_s + "."
+puts "The current temperature is " + current_temp.to_s + "."
